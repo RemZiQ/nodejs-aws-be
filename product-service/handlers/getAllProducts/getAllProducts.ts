502ae1396b0b89e41ddce618fa8712e43655ab56
Yehor Remizov <Yehor.Remizov@coxautoinc.com>
2020-11-09 10:30:34 +0300

    feat: use postgres in getAllProducts.

--- product-service/handlers/getAllProducts/getAllProducts.ts
+++ product-service/handlers/getAllProducts/getAllProducts.ts
@@ -1,12 +1,49 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { Client, ClientConfig } from 'pg';
 import 'source-map-support/register';
-import products from '../../mocks/products.json';
+import { getAllProductsQuery } from './constants';
 import { getCorsHeaders } from '../../helpers/getCorsHeaders';
 
-export const getAllProducts: APIGatewayProxyHandler = async (_, _context) => {
-  return {
-    statusCode: 200,
-    headers: getCorsHeaders(),
-    body: JSON.stringify(products),
-  };
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+const dbOptions: ClientConfig = {
+  host: PG_HOST,
+  port: parseInt(PG_PORT, 10),
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
+
+export const getAllProducts: APIGatewayProxyHandler = async (event, _context) => {
+  // emulate lambda calling logging
+  console.log(event);
+  // debugging
+  console.log('PG_PORT___', PG_PORT, "typeof___", typeof (PG_PORT));
+  const client = new Client(dbOptions);
+  await client.connect();
+  const corsHeaders = getCorsHeaders();
+  try {
+    const { rows: products } = await client.query(getAllProductsQuery);
+    console.log('___products', products);
+    return {
+      statusCode: 200,
+      headers: getCorsHeaders(),
+      body: JSON.stringify(products),
+    };
+  } catch (e) {
+    // emulate error logging
+    console.error(e);
+    return {
+      statusCode: 500,
+      headers: corsHeaders,
+      body: JSON.stringify({ message: `Internal server error` }),
+    }
+
+  } finally {
+    client.end();
+  }
 }
